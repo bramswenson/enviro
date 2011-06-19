03cde13044a8838e1513e418ae96ead486786c76
Bram Swenson <bram.swenson@akqa.com>
2011-06-18 18:44:10 -0700

    [SPEC] fixed version spec

--- spec/enviro/version_spec.rb
+++ spec/enviro/version_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Enviro::VERSION do
-  it "should be 0.0.4" do
-    Enviro::VERSION.should == '0.0.4'
+  it "should be 0.0.5" do
+    Enviro::VERSION.should == '0.0.5'
   end
 end
